4c1654e014f5abc5c249bef243bf5715a1d945a3
Yusuf Çırak <yusuf10901@hotmail.com>
2024-07-06 01:14:21 +0300

    test: adding fixtures

---
+++ test/BookPlatform.WebAPI.IntegrationTests/Fixtures/Base.cs
@@ -0,0 +1,10 @@
+﻿namespace BookPlatform.WebAPI.IntegrationTests.Fixtures;
+
+[CollectionDefinition("BaseCollection")]
+public sealed class BaseCollection : ICollectionFixture<BaseFixture>;
+
+public class BaseFixture
+{
+    public CustomWebApplicationFactory<Program> Factory { get; } = new();
+    public string Token { get; set; }
+}
---
+++ test/BookPlatform.WebAPI.IntegrationTests/Fixtures/BookNoteFixture.cs
@@ -0,0 +1,9 @@
+﻿namespace BookPlatform.WebAPI.IntegrationTests.Fixtures;
+
+public sealed class BookNoteFixture : BaseFixture
+{
+    public string BookNoteId { get; set; }
+}
+
+[CollectionDefinition("BookNoteCollection")]
+public sealed class BookNoteCollectionFixture : ICollectionFixture<BookNoteFixture>;
